03334a29dee5cf910569c7291d4c6d9d2ea23b55
Pingshun Huang <alex.g.huang@gmail.com>
2022-01-17 03:35:17 +1100

    Filter non-Chinese character candidate in repeat history
    Signed-off-by: Pingshun Huang <alex.g.huang@gmail.com>

--- lua/sphs_filter.lua
+++ lua/sphs_filter.lua
@@ -1,43 +1,66 @@
--- ❖
-local function startswith(str, start)
-    return string.sub(str, 1, string.len(start)) == start
+local function starts_with(str, start)
+    return str:sub(1, #start) == start
+end
+
+local function has_cn_char(text)
+    for pos, code in utf8.codes(text) do
+        local char = utf8.char(code)
+        local byte1 = char:byte(1)
+        local byte2 = char:byte(2)
+        local byte3 = char:byte(3)
+
+        if (not byte1) or (not byte2) or (not byte3) then
+            goto continue
+        end
+
+        -- Chinese characters range from 0x4E00 to 0x9FA5 in Unicode, aka from [228, 184, 128] to [233, 190, 165] in UTF-8.
+        -- As a rule of thumb, any characters within [228-233, 128-191, 128-191] can be assumed as Chinese.
+        -- https://titanwolf.org/Network/Articles/Article?AID=038ec1a2-6ed1-49ac-9bf2-e1b00c376d43
+        if (byte1 >= 228 and byte1 <= 233) and (byte2 >= 128 and byte2 <= 191) and (byte3 >= 128 and byte3 <= 191) then
+            return true
+        end
+
+        ::continue::
+    end
+
+    return false
 end
 
 local function hint(cand, context, reverse)
-    if utf8.len(cand.text) < 2 then
-        return false
-    end
+    local phonetics_keycode = "[bcdefghjklmnpqrstwxyz]"
+    local stroke_keycodes = "[aiouv]+"
 
     local lookup = " " .. reverse:lookup(cand.text) .. " "
-    local short = string.match(lookup, " ([bcdefghjklmnpqrstwxyz][auiov]+) ") or
-                  string.match(lookup, " ([bcdefghjklmnpqrstwxyz][bcdefghjklmnpqrstwxyz]) ")
+    local short = lookup:match(" (".. phonetics_keycode .. stroke_keycodes .. ") ") or
+                  lookup:match(" (".. phonetics_keycode .. phonetics_keycode .. ") ")
     local input = context.input
-    if short and utf8.len(input) > utf8.len(short) and not startswith(short, input) then
+
+    if short and #input > #short and not starts_with(short, input) then
         cand:get_genuine().comment = cand.comment .. " [" .. short .. "]"
-        return true
     end
-
-    return false
 end
 
-local function danzi(cand)
-    if utf8.len(cand.text) < 2 then
-        return true
-    end
-    return false
-end
+local function sphs_filter(cand_list, env)
+    local context = env.engine.context
+    local reverse = env.reverse
+    local input = context.input
 
-local function filter(input, env)
-    local is_danzi = env.engine.context:get_option('danzi_mode')
-    local is_on = env.engine.context:get_option('sbb_hint')
+    local is_sbb_hint_on = context:get_option("sbb_hint")
+    local repeat_keycode = ";;" -- sync with repeat_history/input in sphs.schema.yaml
+    local is_repeat_on = starts_with(input, repeat_keycode)
 
-    for cand in input:iter() do
-        if not is_danzi or danzi(cand) then
-            if is_on then
-            hint(cand, env.engine.context, env.reverse)
-            end
-            yield(cand)
+    for cand in cand_list:iter() do
+        if cand.text == nil or cand.text == "" or (is_repeat_on and not has_cn_char(cand.text)) then
+            goto continue
         end
+
+        if is_sbb_hint_on and utf8.len(cand.text) > 1 then
+            hint(cand, context, reverse)
+        end
+
+        yield(cand)
+
+        ::continue::
     end
 end
 
@@ -45,4 +68,4 @@ local function init(env)
     env.reverse = ReverseDb("build/sphs.reverse.bin")
 end
 
-return { init = init, func = filter }
+return { init = init, func = sphs_filter }
