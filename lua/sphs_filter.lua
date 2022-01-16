aaa0ed5d59906d8a63682d905e513271cf09d176
Pingshun Huang <alex.g.huang@gmail.com>
2022-01-17 04:02:50 +1100

    Tidy up check conditions in filter script
    Signed-off-by: Pingshun Huang <alex.g.huang@gmail.com>

--- lua/sphs_filter.lua
+++ lua/sphs_filter.lua
@@ -9,14 +9,16 @@ local function has_cn_char(text)
         local byte2 = char:byte(2)
         local byte3 = char:byte(3)
 
-        if (not byte1) or (not byte2) or (not byte3) then
+        if not byte1 or not byte2 or not byte3 then
             goto continue
         end
 
         -- Chinese characters range from 0x4E00 to 0x9FA5 in Unicode, aka from [228, 184, 128] to [233, 190, 165] in UTF-8.
         -- As a rule of thumb, any characters within [228-233, 128-191, 128-191] can be assumed as Chinese.
         -- https://titanwolf.org/Network/Articles/Article?AID=038ec1a2-6ed1-49ac-9bf2-e1b00c376d43
-        if (byte1 >= 228 and byte1 <= 233) and (byte2 >= 128 and byte2 <= 191) and (byte3 >= 128 and byte3 <= 191) then
+        if (byte1 >= 228 and byte1 <= 233) and
+           (byte2 >= 128 and byte2 <= 191) and
+           (byte3 >= 128 and byte3 <= 191) then
             return true
         end
 
@@ -26,36 +28,40 @@ local function has_cn_char(text)
     return false
 end
 
-local function hint(cand, context, reverse)
+local function add_keycode_hint(cand, context, reverse)
     local phonetics_keycode = "[bcdefghjklmnpqrstwxyz]"
     local stroke_keycodes = "[aiouv]+"
 
-    local lookup = " " .. reverse:lookup(cand.text) .. " "
-    local short = lookup:match(" (".. phonetics_keycode .. stroke_keycodes .. ") ") or
-                  lookup:match(" (".. phonetics_keycode .. phonetics_keycode .. ") ")
     local input = context.input
 
-    if short and #input > #short and not starts_with(short, input) then
-        cand:get_genuine().comment = cand.comment .. " [" .. short .. "]"
+    local lookup = " " .. reverse:lookup(cand.text) .. " "
+    local keycode_hint = lookup:match(" (".. phonetics_keycode .. stroke_keycodes .. ") ") or
+                         lookup:match(" (".. phonetics_keycode .. phonetics_keycode .. ") ")
+
+    if keycode_hint and #input > #keycode_hint and
+       not starts_with(keycode_hint, input) then
+        cand:get_genuine().comment = cand.comment .. " [" .. keycode_hint .. "]"
     end
 end
 
 local function sphs_filter(cand_list, env)
+    local repeat_keycode = ";;" -- sync with repeat_history/input in sphs.schema.yaml
+
     local context = env.engine.context
     local reverse = env.reverse
     local input = context.input
 
     local is_sbb_hint_on = context:get_option("sbb_hint")
-    local repeat_keycode = ";;" -- sync with repeat_history/input in sphs.schema.yaml
     local is_repeat_on = starts_with(input, repeat_keycode)
 
     for cand in cand_list:iter() do
-        if cand.text == nil or cand.text == "" or (is_repeat_on and not has_cn_char(cand.text)) then
+        if cand.text == nil or cand.text == "" or
+           (is_repeat_on and not has_cn_char(cand.text)) then
             goto continue
         end
 
         if is_sbb_hint_on and utf8.len(cand.text) > 1 then
-            hint(cand, context, reverse)
+            add_keycode_hint(cand, context, reverse)
         end
 
         yield(cand)
