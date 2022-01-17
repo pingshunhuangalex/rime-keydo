e7e6e05c0028ada83f3bb700dfc9d7251c6e9a6a
Pingshun Huang <alex.g.huang@gmail.com>
2022-01-17 18:08:45 +1100

    Remove hint support as words with shorthands are no longer duplicated in the main dictionary

--- lua/sphs_cand_filter.lua
+++ lua/sphs_cand_filter.lua
@@ -29,49 +29,13 @@ local function has_cn_char(text)
     return false
 end
 
-local function should_show_hint(shorthand, input)
-    return shorthand and #input >= #shorthand and not starts_with(shorthand, input)
-end
-
-local function add_hints(cand, context, reverse)
-    local phonetics_keycode = "[bcdefghjklmnpqrstwxyz]"
-    local phonetics_keycodes = phonetics_keycode .. phonetics_keycode
-    local stroke_keycodes = "[aiouv]+"
-
-    local input = context.input
-
-    local lookup = " " .. reverse:lookup(cand.text) .. " "
-    local shorthand_sp = lookup:match(" (" .. phonetics_keycode .. stroke_keycodes .. ") ")
-    local shorthand_l2 = lookup:match(" (" .. phonetics_keycodes .. ") ")
-    local shorthand_l3 = lookup:match(" (" .. phonetics_keycode .. phonetics_keycodes .. ") ")
-    local shorthand_l2_l3 = shorthand_l2 or shorthand_l3
-
-    if should_show_hint(shorthand_sp or shorthand_l2_l3, input) then
-        local hints = ""
-
-        if should_show_hint(shorthand_sp, input) then
-            hints = hints .. shorthand_sp
-
-            if should_show_hint(shorthand_l2_l3, input) then
-                hints = hints .. " " .. (shorthand_l2_l3)
-            end
-        else
-            hints = hints .. (shorthand_l2_l3)
-        end
-
-        cand:get_genuine().comment = cand.comment .. " [" .. hints .. "]"
-    end
-end
-
 local function sphs_cand_filter(cand_list, env)
     local engine = env.engine
-    local reverse = env.reverse
 
     local context = engine.context
     local input = context.input
     local config = engine.schema.config
 
-    local is_sbb_hint_on = context:get_option("sbb_hint")
     local repeat_keycode = config:get_string("repeat_history/input")
     local is_repeat_on = starts_with(input, repeat_keycode)
 
@@ -83,18 +47,10 @@ local function sphs_cand_filter(cand_list, env)
             goto continue
         end
 
-        if is_sbb_hint_on and utf8.len(cand.text) > 1 then
-            add_hints(cand, context, reverse)
-        end
-
         yield(cand)
 
         ::continue::
     end
 end
 
-local function init(env)
-    env.reverse = ReverseDb("build/sphs.reverse.bin")
-end
-
-return { init = init, func = sphs_cand_filter }
+return { func = sphs_cand_filter }
