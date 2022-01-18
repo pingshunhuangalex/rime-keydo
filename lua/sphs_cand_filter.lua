71982c5ff3457306ff0ff7e0352021f379931c34
Pingshun Huang <alex.g.huang@gmail.com>
2022-01-19 03:11:01 +1100

    Avoid unnecessary variable calc

--- lua/sphs_cand_filter.lua
+++ lua/sphs_cand_filter.lua
@@ -6,8 +6,9 @@ local function filter(cand_list, env)
     local repeat_keycode = env.repeat_keycode
     local input = env.engine.context.input
 
+    local is_repeat_on = starts_with(input, repeat_keycode)
+
     for cand in cand_list:iter() do
-        local is_repeat_on = starts_with(input, repeat_keycode)
         local is_repeat_cand_invalid = is_repeat_on and not has_cn_char(cand.text)
 
         if not is_valid(cand.text) or is_repeat_cand_invalid then
