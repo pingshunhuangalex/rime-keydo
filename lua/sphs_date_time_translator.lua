4c6b94de7acecb7c94919fb4b2c7d2f2699a5654
Pingshun Huang <alex.g.huang@gmail.com>
2022-03-27 17:44:33 +1100

    Reorder date candidates

--- lua/sphs_date_time_translator.lua
+++ lua/sphs_date_time_translator.lua
@@ -6,8 +6,9 @@ local switch = sphs_modules.switch
 local DATE_TYPE = "date" -- 日期与时间所对应的候选项类型字符串
 
 -- 日期与时间转换器
--- - 获取当前日期（年月日、星期数）
--- - 获取当前时间（时分秒）
+-- - 获取当前日期
+-- - 获取当前星期数
+-- - 获取当前时间
 local function translator(input, seg)
     local start = seg.start -- 高亮选项在输入区字符串中的起始位置
     local _end = seg._end -- 高亮选项在输入区字符串中的结束位置
@@ -15,11 +16,11 @@ local function translator(input, seg)
     -- 日期与时间功能所对应的编码集
     local date_time_table = {
         [";xt"] = function()
-            local time12 = os.date(formatters.time12) -- 当前时间（12小时制）
             local time24 = os.date(formatters.time24) -- 当前时间（24小时制）
+            local time12 = os.date(formatters.time12) -- 当前时间（12小时制）
 
-            yield(Candidate(DATE_TYPE, start, _end, time12, ""))
             yield(Candidate(DATE_TYPE, start, _end, time24, ""))
+            yield(Candidate(DATE_TYPE, start, _end, time12, ""))
         end,
         [";xw"] = function()
             local weekday_index = tonumber(os.date(formatters.week)) -- 当前星期数序号
